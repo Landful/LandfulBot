529c9173751f722a831b06fb292ae4bf5dce8582
nirewen <eduardolonderovieira@gmail.com>
2018-11-14 17:37:19 -0200

    [travis] Fix scale

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,5 +2,5 @@
 
 now --public -e TOKEN='${TOKEN}' -t ${NOW_TOKEN}
 now alias -t ${NOW_TOKEN}
-now scale -t ${NOW_TOKEN}
+now scale landfulbot.now.sh gru 1 auto -t ${NOW_TOKEN}
 now rm landfulbot --yes -t ${NOW_TOKEN}
