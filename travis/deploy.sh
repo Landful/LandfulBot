656d6967506ab90b4c272a7c24b9f80370a25af0
nirewen <eduardolonderovieira@gmail.com>
2018-11-14 18:00:04 -0200

    [travis] Try again?

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
+npm install
+
 now --public -e TOKEN='${TOKEN}' -t ${NOW_TOKEN}
-now alias -t ${NOW_TOKEN}
-now scale landfulbot.now.sh gru 1 auto -t ${NOW_TOKEN}
-now rm landfulbot --yes -t ${NOW_TOKEN}
