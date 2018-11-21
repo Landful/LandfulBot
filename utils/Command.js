29fbadf047b8d69d0d677cd57a2bfa49b7d30dac
Tsugami <yslan3000@gmail.com>
2018-11-21 20:05:49 -0200

    bot-invite.js

--- utils/Command.js
+++ utils/Command.js
@@ -1,20 +1,26 @@
 class Command {
-    constructor (name, client) {
-        this.name = name
-        this.client = client
-        this.aliases = []
-        this.category = 'normal'
-        this.adminOnly = false
+    constructor(name, client) {
+        this.name = name;
+        this.client = client;
+        this.aliases = [];
+        this.category = 'normal';
+        this.argsRequired = false;
+        this.usage = '';
+
+        this.invalidArgsMessage = '';
     }
 
-    process (message, args) {
-        if (this.adminOnly && !message.member.roles.has('318166278881673217'))
-            return
-            
-        return this.run(message, args)
+    get tag() {
+        return `${this.client.prefixes[0]}${this.name} ${this.usage}`;
+    }
+
+    process(message, args) {
+        if (this.argsRequired && args.length === 0)
+            return typeof this.invalidArgsMessage === 'function' ? this.invalidUsageMessage(message, args) : message.channel.send(this.invalidArgsMessage);
+        return this.run(message, args);
     }
 
     run () {}
 }
 
-module.exports = Command
+module.exports = Command;
