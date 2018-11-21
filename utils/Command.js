41449fce49c057a3400d956d11db34eb36a09cb6
Tsugami <yslan3000@gmail.com>
2018-11-21 19:36:16 -0200

    bot-invite.js
    kibando o moru

--- utils/Command.js
+++ utils/Command.js
@@ -4,9 +4,19 @@ class Command {
         this.client = client;
         this.aliases = [];
         this.category = 'normal';
+        this.argsRequired = false;
+        this.usage = '';
+
+        this.invalidArgsMessage = '';
+    }
+
+    get tag() {
+        return `${this.client.prefixes[0]}${this.name} ${this.usage}`;
     }
 
     process(message, args) {
+        if (this.argsRequired && args.length === 0)
+            return typeof this.invalidArgsMessage === 'function' ? this.invalidUsageMessage(message, args) : message.channel.send(this.invalidArgsMessage);
         return this.run(message, args);
     }
 
