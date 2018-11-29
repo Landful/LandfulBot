e360d96ab4aa9b7ef6805fe69926064d4a592560
Tsugami <yslan3000@gmail.com>
2018-11-29 02:02:34 -0200

    autorole

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,9 @@
+const { Constants } = require('../utils');
+class GuildMemberAdd {
+   static on(member) {
+        if (!member.user.bot) member.roles.add(Constants.MEMBERS_ROLE);
+            else member.roles.add(Constants.BOTS_ROLE);
+    }
+}
+
+module.exports = GuildMemberAdd.on;
--- utils/Constants.js
+++ utils/Constants.js
@@ -1,5 +1,9 @@
 module.exports = {
+
+    // roles ids
     STAFF_ROLE: "318166278881673217",
+    BOTS_ROLE: "278642797798752256" ,
+    MEMBERS_ROLE: "493619491457073164",
     
     VILLAGER_PNG: './src/image/villager-face.png'
 }
