c279096f91419cf103bd50e62b79789b7eec61be
nirewen <eduardolonderovieira@gmail.com>
2018-12-04 13:11:23 -0200

    Fix image

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -7,7 +7,7 @@ module.exports = async function onMessageReactionAdd (reaction) {
         let { message, users } = reaction
         let emoji = users.size >= 1 ? emojis[0] : users.size >= 5 ? emojis[1] : emojis[2]
         let channel = message.guild.channels.get('519526821214289950')
-        let image = Object.values(message.embeds[0]).filter(k => k).find(r => r.url)
+        let image = Object.values(message.embeds[0] || {}).filter(k => k).find(r => r.url)
         let embed = new MessageEmbed()
             .setAuthor(message.author.username, message.author.avatarURL(), message.url)
             .setDescription(message.content)
