a63f4c2d05b405b1281d781ea31cd6b1885b546b
nirewen <eduardolonderovieira@gmail.com>
2018-12-04 13:03:47 -0200

    Add starboard system

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require('discord.js')
+
+const emojis = ['\u2B50', '\u1F31F', '\u1F320']
+
+module.exports = async function onMessageReactionAdd (reaction) {
+    if (reaction.emoji.name === '\u2B50') { // star emoji
+        let { message, users } = reaction
+        let emoji = users.size >= 1 ? emojis[0] : users.size >= 5 ? emojis[1] : emojis[2]
+        let channel = message.guild.channels.get('519526821214289950')
+        let image = Object.values(message.embeds[0]).filter(k => k).find(r => r.url)
+        let embed = new MessageEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL(), message.url)
+            .setDescription(message.content)
+            .setImage(image ? image.url : null)
+            .setColor('YELLOW')
+        let msg
+
+        if (users.size > 1)
+            msg = await channel.messages
+                .fetch({ limit: 200 })
+                .find(m => 
+                    m.embeds[0] && 
+                    m.embeds[0].author &&
+                    m.embeds[0].author.url.includes(message.id))
+
+        if (!msg)
+            await channel.send(`${emoji} ${users.size}`, embed)
+        else
+            msg.edit(`${emoji} ${users.size}`, embed)
+    }
+}
