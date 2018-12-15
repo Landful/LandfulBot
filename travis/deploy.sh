8e01519290180e7cbfa7a9a0fc61ded734c6d955
nirewen <eduardolonderovieira@gmail.com>
2018-12-15 21:07:22 -0200

    Fix scale

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -7,4 +7,4 @@ npm install now --no-save
 now rm landfulbot -y -t ${NOW_TOKEN}
 now -t ${NOW_TOKEN}
 now alias -t ${NOW_TOKEN}
-now scale -t ${NOW_TOKEN}
+now scale landfulbot.now.sh sfo1 1 1 -t ${NOW_TOKEN}
