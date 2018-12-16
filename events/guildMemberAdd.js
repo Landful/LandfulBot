b5e6e2999eb811528998aa5d101197b213312776
Tsugami <yslan3000@gmail.com>
2018-12-16 02:17:58 -0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,6 @@
-class GuildMemberAdd {
-    static on (member) {
-        if (!member.user.bot) 
-            member.roles.add(process.env.MEMBERS_ROLE)
-        else 
-            member.roles.add(process.env.BOTS_ROLE)
-    }
+module.exports = function GuildMemberAdd (member) {
+    if (!member.user.bot) 
+        member.roles.add(process.env.MEMBERS_ROLE)
+    else 
+        member.roles.add(process.env.BOTS_ROLE)
 }
-
-module.exports = GuildMemberAdd.on
